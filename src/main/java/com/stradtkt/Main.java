477530b3f193adc230b5dfbc38b270aef51b59ec
Kevin Stradtman <stradtkt22@gmail.com>
2020-09-30 10:29:55 -0400

    Initial Commit

---
+++ src/main/java/com/stradtkt/Main.java
@@ -0,0 +1,11 @@
+package com.stradtkt;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private final static Logger log = LoggerFactory.getLogger(Main.class);
+    public static void main(String[] args) {
+        System.out.println("Hello Maven");
+    }
+}
